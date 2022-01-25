2184350fc689e8023df170622df5d6974f6dcbd1
Boierul <49337867+Boierul@users.noreply.github.com>
2022-01-25 14:45:04 +0100

    Triple H

--- GraphQlAPI/Data/Interface/ITransaction.cs
+++ GraphQlAPI/Data/Interface/ITransaction.cs
@@ -6,8 +6,8 @@ namespace GraphQlAPI.Data
 {
     public interface ITransaction
     {
-        Task<double> Deposit(double Amount);
-        Task<double> Withdraw(double Amount);
-        Task<IList<Transaction>> getLast5Transactions();
+        Task<double> Deposit(int id, double Amount);
+        Task<double> Withdraw(int id, double Amount);
+        Task<IList<Transaction>> getLast5Transactions(int accountId);
     }
 }
--- GraphQlAPI/Data/TransactionImpl.cs
+++ GraphQlAPI/Data/TransactionImpl.cs
@@ -1,28 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using GraphQlAPI.Model;
 using GraphQlAPI.Persistence;
+using Microsoft.EntityFrameworkCore;
 
 namespace GraphQlAPI.Data
 {
     public class TransactionImpl : ITransaction
     {
         private readonly ATMContext context;
+        // private Transaction transaction;
+        
+        public async Task<double> Deposit(int id, double Amount)
+        {
+            var ruble = await context.Transactions.Where(t
+                => t.id == id).FirstOrDefaultAsync();
+            ruble.Amount == Amount;
+            
+            await context.SaveChangesAsync();
+            return 0;
+        }
 
-        public Task<double> Deposit(double Amount)
+        public async Task<double> Withdraw(int id, double Amount)
         {
             throw new System.NotImplementedException();
         }
 
-        public Task<double> Withdraw(double Amount)
+        public async Task<IList<Transaction>> getLast5Transactions(int accountId)
         {
-            throw new System.NotImplementedException();
-        }
+            var acc = await context.Accounts.Where(a
+                => a.id == accountId).FirstOrDefaultAsync();
 
-        public Task<IList<Transaction>> getLast5Transactions()
-        {
-            throw new System.NotImplementedException();
+            context.Transactions.AddAsync(acc);
+
+            // throw new System.NotImplementedException();
         }
     }
 }
